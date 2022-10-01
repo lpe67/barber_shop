6633800205dc756989f6ccdbc6cc0e7510b888a3
lpe67 <44515207+lpe67@users.noreply.github.com>
2022-10-01 04:00:35 -0300

    Made the top box actually appear yay!!!! time to actually do the app tomorrow

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,11 +7,14 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Container(
-        margin: const EdgeInsets.only(left: 20, right: 20, top: 0),
+        margin:
+            const EdgeInsets.only(left: 100, right: 100, top: 0, bottom: 550),
         decoration: const BoxDecoration(
           borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(4), bottomRight: Radius.circular(4)),
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20)),
           color: Colors.white,
         ),
       ),
