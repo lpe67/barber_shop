33507560bf411b2b9f50a9dd1a9f9c6f2bf001a8
lpe67 <44515207+lpe67@users.noreply.github.com>
2022-10-01 03:38:35 -0300

    Removed flutter base main added somethings for the project added a weird box and thats that

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  final titleStyle = TextStyle(
+      color: Colors.black, fontSize: 10, fontWeight: FontWeight.normal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 20, right: 20, top: 0),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(4), bottomRight: Radius.circular(4)),
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
